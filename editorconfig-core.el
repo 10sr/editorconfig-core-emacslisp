d3a196b6bbc94b9c9b8fec773d62120952cb1351
10sr <8slashes+git@gmail.com>
2015-09-26 15:03:15 +0900

    Add some test cases

--- editorconfig-core.el
+++ editorconfig-core.el
@@ -1,3 +1,10 @@
+(defun editorconfig-core-wildcard-match-p (target pattern &optional dir)
+  "Test if PATTERN  matches with TERGET filename.
+
+Optional third argument DIR specifies the base directory for PATTERN, which
+defaults to `default-directory'."
+  t)
+
 (defun editorconfig-core-get-properties (file &optional confname version)
   "Get EditorConfig properties for FILE.
 
--- tests-ert/editorconfig-core.el
+++ tests-ert/editorconfig-core.el
@@ -1,4 +1,26 @@
 (require 'editorconfig-core)
 
-(ert-deftest editorconfig-core-wildcard-match ()
-  (should t))
+
+
+(ert-deftest test-editorconfig-core-wildcard-match-p ()
+  (should (editorconfig-core-wildcard-match-p "a.js"
+                                              "*.js"))
+  (should (not (editorconfig-core-wildcard-match-p "a.js"
+                                                   "*.py")))
+  (should (editorconfig-core-wildcard-match-p "/repository/root/a.js"
+                                              "/repository/root/*.js"))
+  (should (editorconfig-core-wildcard-match-p "/repository/root/a.js"
+                                              "*.js"))
+  (should (editorconfig-core-wildcard-match-p "/repository/root/sub/a.js"
+                                              "*.js"
+                                              "/repository/root/"))
+  (should (not (editorconfig-core-wildcard-match-p "/repository/root/sub/sub/a.js"
+                                                   "*.js"
+                                                   "/repository/root/")))
+  (should (editorconfig-core-wildcard-match-p "a.js"
+                                              "*.js"))
+  (should (editorconfig-core-wildcard-match-p "a.js"
+                                              "*.js"))
+  (should (editorconfig-core-wildcard-match-p "a.js"
+                                              "*.js"))
+  )
