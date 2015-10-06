153638e0472accff3668ca28c2b66889ef20d696
10sr <8slashes+git@gmail.com>
2015-10-06 13:07:24 +0900

    Implement merge-two-properties

--- editorconfig-core.el
+++ editorconfig-core.el
@@ -3,32 +3,47 @@
 (require 'editorconfig-core-handle)
 
 
-(defun editorconfig-core--merge-properties (list)
-  "Merge LIST, which has property alist as its element.
+(defun editorconfig-core--merge-properties (current &rest rest)
+  "Merge CURRENT and REST, which are alists of properties.
 
-The first property alist takes precedence. For examle, when LIST is
+Latter property alists take precedence. For examle, when called like
 
-'(
-  ((a . 2) (c . 2))
-  ((a . 1) (b . 1))
-)
+(editorconfig-core--merge-properties '((a . 1) (c . 1))
+                                     '((a . 2) (b . 2)))
 
 then the result will be
 
 '(
-  ((a . 2) (b . 1) (c . 2))
-)."
-  (if (cdr list)
-      (editorconfig-core--merge-two-properties (car list)
-                                               (editorconfig-core--merge-properties (cdr list)))
-    (car list)))
+  (a . 2) (b . 2) (c . 1)
+  ) ."
+  (if rest
+      (editorconfig-core--merge-properties (editorconfig-core--merge-two-properties current (car rest))
+                                           (cdr rest))
+    current))
 
-(defun editorconfig-core--merge-two-properties (new old)
-  "Merge two property alist.")
+(defun editorconfig-core--merge-two-properties (old new)
+  "Merge two property alist.
+Properties in OLD will be overwritten by properties in NEW.
+This function is non-destructive."
+  (if old
+      (let ((result (copy-alist old)))
+        (dolist (e new)
+          (let ((pair (assoc (car e)
+                             result)))
+            (if pair
+                (setcdr pair
+                        (cdr e))
+              (setq result
+                    `(,@result ,e)))))
+        result)
+
+    ;; If OLD is nil return NEW as it is
+    (copy-alist new)))
 
 (defun editorconfig-core-get-properties-from (file conf)
   "Get EditorConfig properties for FILE from CONF.")
 
+;;;###autoload
 (defun editorconfig-core-get-properties (file &optional confname version)
   "Get EditorConfig properties for FILE.
 
